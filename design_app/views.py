4b6ba5d0f9a1cd17b95c01174ff922601bacbbca
alexmitroff <alexmitroff@gmail.com>
2017-01-16 20:51:23 +0300

    added news through ajax

--- design_app/views.py
+++ design_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from egd_prj import settings
+from django.http import JsonResponse
+
+from design_app.models import *
+import json
+
+
 
 # Create your views here.
 
@@ -12,3 +18,22 @@ def news(request):
     template = 'pages/news.html'
     var = {}
     return render(request, template, var)
+
+def feed(request):
+    l = request.LANGUAGE_CODE
+    if 'category' in request.GET:
+        news = NewsItem.objects.filter(publish=True, 
+                category=request.GET['category'])
+    else:
+        news = NewsItem.objects.filter(publish=True)
+
+    if l == 'ru':
+        data = [{'date':item.created,'color':item.category.color,
+            'category':item.category.name_ru,'title': item.title_ru, 
+            'short': item.content_ru} for item in news]
+    else:
+        data = [{'date':item.created,'color':item.category.color,
+            'category':item.category.name_en,'title': item.title_en, 
+            'short': item.content_en} for item in news]
+
+    return JsonResponse(data, safe=False)
--- egd_prj/urls.py
+++ egd_prj/urls.py
@@ -1,17 +1,9 @@
-"""egd_prj URL Configuration
+"""
+egd_prj URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+URLs includes API.
+Well it is not REST, but some sort of.
+It returns JSON.
 """
 from django.conf.urls import url
 from django.conf.urls import include
@@ -23,10 +15,15 @@ from django.conf import settings
 from django.contrib.flatpages import views as fp_views
 from design_app import views
 
+api_patterns = [
+    url(r'feed/$', views.feed, name='feed'),
+]
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^api/', include(api_patterns)),
 ]
 
 urlpatterns += i18n_patterns(
